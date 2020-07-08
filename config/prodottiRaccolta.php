c6491e73cba846989472151f637b3efe519b098f
sava-labriola <labriolasava00@gmail.com>
2020-07-08 11:07:03 +0200

    migration

---
+++ config/prodottiRaccolta.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    [
+        'nome'=> 'tv',
+        'descrizione'=> 'led',
+        'prezzo'=> '799.99',
+    ],
+    [
+        'nome'=> 'frigorifero',
+        'descrizione'=> 'grigio',
+        'prezzo'=> '399.99',
+    ],
+    [
+        'nome'=> 'padella',
+        'descrizione'=> 'antiaderente',
+        'prezzo'=> '59.99',
+    ],
+];
+
+?>
